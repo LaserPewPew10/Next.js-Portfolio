2cf275620ec613f5633a63eadd100539a712cbf9
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-11 14:56:37 -0400

    added in header and pages for start of portfolio

---
+++ pages/cv.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CV extends React.Component {
+  render() {
+    return <h1>I am CV Page</h1>;
+  }
+}
+
+export default CV;
---
+++ pages/portfolio.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Portfolio extends React.Component {
+  render() {
+    return <h1>I am Portfolio Page</h1>;
+  }
+}
+
+export default Portfolio;
