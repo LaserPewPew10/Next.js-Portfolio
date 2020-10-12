560988d3dd954fe27e5b4716b77c3ea7c235cd4c
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-12 16:05:46 -0400

    post data within portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,30 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
+import axios from "axios";
 
 class Portfolio extends React.Component {
+  static async getIntialProps() {
+    let posts = [];
+    try {
+      const res = await axios.get("http://jsonplaceholder.typicode.com/posts");
+      posts = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+
+    return { posts: posts.slice(0, 10) };
+  }
+
+  renderPosts(posts) {
+    return posts.map((post) => <li key={post.id}>{post.id}</li>);
+  }
+
   render() {
+    const { posts } = this.props;
     return (
       <BaseLayout>
         <h1>I am Portfolio Page</h1>
+        <ul>{this.renderPosts(posts)}</ul>
       </BaseLayout>
     );
   }
