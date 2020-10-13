8a7a10bd65cb76b680690297247ef1e4149a8a6d
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-13 17:42:19 -0400

    adding in more functionality and styling for navbar

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -6,95 +6,58 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
 } from "reactstrap";
 
 import Link from "next/link";
-
-// const Example = (props) => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggle = () => setIsOpen(!isOpen);
-
-//   return (
-//     <div>
-// <Navbar color="light" light expand="md">
-//   <NavbarBrand href="/">reactstrap</NavbarBrand>
-//   <NavbarToggler onClick={toggle} />
-//   <Collapse isOpen={isOpen} navbar>
-//     <Nav className="mr-auto" navbar>
-//       <NavItem>
-//         <NavLink href="/components/">Components</NavLink>
-//       </NavItem>
-//       <NavItem>
-//         <NavLink href="https://github.com/reactstrap/reactstrap">
-//           GitHub
-//         </NavLink>
-//       </NavItem>
-//       <UncontrolledDropdown nav inNavbar>
-//         <DropdownToggle nav caret>
-//           Options
-//         </DropdownToggle>
-//         <DropdownMenu right>
-//           <DropdownItem>Option 1</DropdownItem>
-//           <DropdownItem>Option 2</DropdownItem>
-//           <DropdownItem divider />
-//           <DropdownItem>Reset</DropdownItem>
-//         </DropdownMenu>
-//       </UncontrolledDropdown>
-//     </Nav>
-//     <NavbarText>Simple Text</NavbarText>
-//   </Collapse>
-// </Navbar>
-//     </div>
-//   );
-// };
-
-// export default Example;
 
 const BsNavLink = (props) => {
   const { title, href } = props;
   return (
     <Link href={href}>
-      <a className="nav-link">{title}</a>
+      <a className="nav-link port-navbar-link">{title}</a>
     </Link>
   );
 };
 
-export default class Header extends React.Component {
-  state = { isOpen: false };
-  toggle = () => this.setState({ isOpen: !this.state.isOpen });
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  render() {
-    return (
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <BsNavLink href="/" title="Home" />
-            </NavItem>
-            <NavItem>
-              <BsNavLink href="/about" title="About" />
-            </NavItem>
-            <NavItem>
-              <BsNavLink href="/portfolios" title="Portfolios" />
-            </NavItem>
-            <NavItem>
-              <BsNavLink href="/blogs" title="Blogs" />
-            </NavItem>
-            <NavItem>
-              <BsNavLink href="/cv" title="CV" />
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
-  }
-}
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar
+      className="port-navbar port-default absolute"
+      color="transparent"
+      dark
+      expand="md"
+    >
+      <NavbarBrand>
+        <Link href="/">
+          <a className="port-navbar-brand">Justin Ramirez</a>
+        </Link>
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem className="port-navbar-item">
+            <BsNavLink href="/" title="Home" />
+          </NavItem>
+          <NavItem className="port-navbar-item">
+            <BsNavLink href="/about" title="About" />
+          </NavItem>
+          <NavItem className="port-navbar-item">
+            <BsNavLink href="/portfolios" title="Portfolios" />
+          </NavItem>
+          <NavItem className="port-navbar-item">
+            <BsNavLink href="/blogs" title="Blogs" />
+          </NavItem>
+          <NavItem className="port-navbar-item">
+            <BsNavLink href="/cv" title="CV" />
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
