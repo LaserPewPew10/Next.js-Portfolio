f2e0655d178aeb7f17dfaf194f57f359ae4ea2f3
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-13 19:31:51 -0400

    adding in functional changes for portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/main.scss";
+import "@/styles/main.scss";
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
