3d892002967204a30d4c5cdcdc9bb480e9e27eac
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-15 14:02:57 -0400

    started to add env variables

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -18,9 +18,7 @@ const BsNavBrand = () => (
   </Link>
 );
 
-const LoginLink = () => (
-  <span className="nav-link port-navbar-link clickable">Login</span>
-);
+const LoginLink = () => <BsNavLink href="/api/v1/login" title="Login" />;
 
 const LogoutLink = () => (
   <span className="nav-link port-navbar-link clickable">Logout</span>
