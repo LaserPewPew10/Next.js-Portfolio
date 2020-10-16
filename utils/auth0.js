e9aad25ef5c9d8675240ab2aeb4e759dc4815877
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-16 17:30:43 -0400

    added in admin rights for portfolio

--- utils/auth0.js
+++ utils/auth0.js
@@ -15,6 +15,10 @@ const auth0 = initAuth0({
 
 export default auth0;
 
+export const isAuthorized = (user, role) => {
+  return user && user["https://portfolio-justin.com" + "/roles"].includes(role);
+};
+
 export const authorizeUser = async (req, res) => {
   const session = await auth0.getSession(req);
   if (!session || !session.user) {
