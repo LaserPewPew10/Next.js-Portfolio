7dfccdf477e54f418d4146d43228ae08ae3bfcbe
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-16 15:13:06 -0400

    started to implement high order components

--- pages/secret.js
+++ pages/secret.js
@@ -2,8 +2,9 @@ import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import { useGetUser } from "@/actions/user";
 import Redirect from "@/components/shared/Redirect";
+import withAuth from "@/hoc/withAuth";
 
-const Secret = () => {
+const Secret = ({ title }) => {
   const { data, loading } = useGetUser();
 
   if (loading) {
@@ -16,11 +17,25 @@ const Secret = () => {
     return (
       <BaseLayout user={data} loading={loading}>
         <BasePage>
-          <h1>I am Secret Page</h1>
+          <h1>I am Secret Page - {title}</h1>
         </BasePage>
       </BaseLayout>
     );
   }
 };
 
-export default Secret;
+// High Order Component = HOC
+// Simple function that takes a component and returns new component
+// component with some extended functionality
+
+// function withAuth(Component) {
+//   return function (props) {
+//     return <Component title="Hello World" {...props} />;
+//   };
+// }
+
+// const withAuth = (Component) => (props) => (
+//   <Component title="Hello World" {...props} />
+// );
+
+export default withAuth(Secret);
