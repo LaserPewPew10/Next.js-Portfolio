f758c938b6ce16d8ba22030fb937e22e92b7a021
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-27 20:26:31 -0400

    adding in state and useeffect for date changes

--- components/PortfolioForm.js
+++ components/PortfolioForm.js
@@ -1,8 +1,22 @@
 import DatePicker from "react-datepicker";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const PortfolioForm = ({ onSubmit }) => {
-  const { register, handleSubmit } = useForm();
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const { register, handleSubmit, setValue } = useForm();
+
+  useEffect(() => {
+    register({ name: "startDate" });
+    register({ name: "endDate" });
+  }, [register]);
+
+  const handleDateChange = (dateType, setDate) => (date) => {
+    setValue(dateType, date);
+    setDate(date);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group">
@@ -77,8 +91,8 @@ const PortfolioForm = ({ onSubmit }) => {
         <div>
           <DatePicker
             showYearDropdown
-            selected={new Date()}
-            onChange={() => {}}
+            selected={startDate}
+            onChange={handleDateChange("startDate", setStartDate)}
           />
         </div>
       </div>
@@ -88,8 +102,8 @@ const PortfolioForm = ({ onSubmit }) => {
         <div>
           <DatePicker
             showYearDropdown
-            selected={new Date()}
-            onChange={() => {}}
+            selected={endDate}
+            onChange={handleDateChange("endDate", setEndDate)}
           />
         </div>
       </div>
