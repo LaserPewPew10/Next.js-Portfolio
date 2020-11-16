70c0e266ceb526c16348b52a3fb2f359020418a4
Justin Ramirez <ramirezbjustin@gmail.com>
2020-11-16 17:22:00 -0500

    adding in portfolio detail page

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -9,11 +9,28 @@ const Portfolio = ({ portfolio }) => {
 
    // TODO: Provide proper stylings!!!
   return (
-    <BaseLayout user={dataU} loading={loadingU}>
+    <BaseLayout
+    navClass="transparent"
+    user={dataU} loading={loadingU}>
        <BasePage
+        noWrapper
+        indexPage
         title={`${portfolio.title} - Justin Ramirez`}
-        header="Portfolio Detail"
-        metaDescription={portfolio.description}>{JSON.stringify(portfolio)}</BasePage>
+        metaDescription={portfolio.description}>
+            <div className="portfolio-detail">
+          <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <main role="main" class="inner page-cover">
+              <h1 class="cover-heading">Title</h1>
+              <p class="lead dates">dates</p>
+              <p class="lead info mb-0">jobTitle | company | location</p>
+              <p class="lead">description</p>
+              <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary">Visit Company</a>
+              </p>
+            </main>
+          </div>
+        </div>
+        </BasePage>
     </BaseLayout>
   );
 };
