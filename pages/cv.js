5e4fa73ad6d4e6134950e08e344dc5336ce549d7
Justin Ramirez <ramirezbjustin@gmail.com>
2021-03-04 14:45:57 -0500

    adding in new resume for cv

--- pages/cv.js
+++ pages/cv.js
@@ -1,20 +1,22 @@
-import React from "react";
-import BaseLayout from "@/components/layouts/BaseLayout";
-import BasePage from "@/components/BasePage";
-import { useGetUser } from "@/actions/user";
-import {Row, Col } from 'reactstrap';
+import React from 'react';
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import { useGetUser } from '@/actions/user';
+import { Row, Col } from 'reactstrap';
 
 const CV = () => {
   const { data, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
-        <BasePage
-        title="My Experiences - Justin Ramirez">
-      <Row>
-        <Col md={{size: 8, offset:2}}>
-          <iframe style={{width: "100%", height: "800px"}} src="/software_resume.pdf"></iframe>
-        </Col>
-      </Row>
+      <BasePage title="My Experiences - Justin Ramirez">
+        <Row>
+          <Col md={{ size: 8, offset: 2 }}>
+            <iframe
+              style={{ width: '100%', height: '800px' }}
+              src="/Justin Ramirez.pdf"
+            ></iframe>
+          </Col>
+        </Row>
       </BasePage>
     </BaseLayout>
   );
